91ce974ea445b385fcf48c9246d3b35bf84a6b79
carryjian <1370437353@qq.com>
2018-01-29 21:52:10 +0800

    '学习python第一步'

---
+++ hello.py
@@ -0,0 +1,19 @@
+
+d = {'jack':89,'tom':90,'rose':100}
+print(d)
+print(d['tom'])
+
+d['admin'] = 67
+print(len(d))
+print(d['admin'])
+print(d.get('jack1'))
+
+def my_abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+print(my_abs(-100.9))
+#ax^2 + bx + c = 0
+
+
