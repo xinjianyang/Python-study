3da04543ee9a701e1e78de819be04fe69160fe03
carryjian <1370437353@qq.com>
2018-01-29 21:59:39 +0800

    '关于字符编码问题'

--- hello.py
+++ hello.py
@@ -16,4 +16,7 @@ def my_abs(x):
 print(my_abs(-100.9))
 #ax^2 + bx + c = 0
 
+print("在计算机内存中，统一使用Unicode编码，当需要保存到硬盘或者需要传输的时候，就转换为UTF-8编码。")
 
+print("用记事本编辑的时候，从文件读取的UTF-8字符被转换为Unicode字符到内存里，编辑完成后，保存的时候再把Unicode转换为UTF-8保存到文件")
+print("浏览网页的时候，服务器会把动态生成的Unicode内容转换为UTF-8再传输到浏览器：")
